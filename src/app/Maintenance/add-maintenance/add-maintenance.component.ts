6b6b1eccc70f78152ea077cadf0933eda69eb45b
HDLLakshan <HDLLakshan@users.noreply.github.com>
2019-08-18 00:35:26 +0530

    I add my add-functions and nev-bar 2nd update.. forget select files

---
+++ src/app/Maintenance/add-maintenance/add-maintenance.component.ts
@@ -0,0 +1,57 @@
+import {Component, NgModule, OnInit, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import {MatAutocompleteModule, MatFormFieldModule, MatInputModule, MatSelectModule} from '@angular/material';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {BrowserModule} from '@angular/platform-browser';
+import {FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {Observable} from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
+import {AppComponent} from '../../app.component';
+
+@Component({
+  selector: 'app-add-maintenance',
+  templateUrl: './add-maintenance.component.html',
+  styleUrls: ['./add-maintenance.component.css']
+})
+@NgModule({
+  declarations: [
+    AppComponent,
+    AddMaintenanceComponent,
+  ],
+  imports: [BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatAutocompleteModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    BrowserAnimationsModule,
+  ],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA,
+  ],
+})
+
+export class AddMaintenanceComponent implements OnInit {
+
+  myControl = new FormControl();
+  options: string[] = ['One', 'Two', 'Three'];
+  filteredOptions: Observable<string[]>;
+
+  ngOnInit() {
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value))
+    );
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.options.filter(option => option.toLowerCase().indexOf(filterValue) === 0);
+  }
+
+  log(x) { console.log(x); }
+
+}
