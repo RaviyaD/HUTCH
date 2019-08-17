03897a9f7122f36003e1ca4f22c7cb931828d177
HDLLakshan <HDLLakshan@users.noreply.github.com>
2019-08-18 00:02:48 +0530

    I add my add-functions and nev-bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {SpecificationComponent} from './specification/specification.component';
+import { AddMaintenanceComponent} from './Maintenance/add-maintenance/add-maintenance.component';
 
+const routes: Routes = [{path: 'specification', component: SpecificationComponent },
+  {path: 'add-maintenance', component: AddMaintenanceComponent}];
 
-const routes: Routes = [{path: 'specification', component: SpecificationComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {Component, NgModule} from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SpecificationComponent } from './specification/specification.component';
 import {RouterModule} from '@angular/router';
 import { SideNavComponent } from './side-nav/side-nav.component';
+import { AddMaintenanceComponent } from './Maintenance/add-maintenance/add-maintenance.component';
+import {MatAutocompleteModule, MatInputModule, MatSelectModule} from '@angular/material';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
     SpecificationComponent,
-    SideNavComponent
+    SideNavComponent,
+    AddMaintenanceComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule
+    RouterModule,
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatSelectModule,
+    MatAutocompleteModule,
+    MatInputModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
