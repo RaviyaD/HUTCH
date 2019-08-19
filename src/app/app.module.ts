988e5df97c1c85ee4731d806f1f034800530f9ce
HDLLakshan <HDLLakshan@users.noreply.github.com>
2019-08-19 12:33:24 +0530

    Solved Venuri's Errors...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import {Component, NgModule} from '@angular/core';
-
+import {MapMainComponent} from './SiteMap/map-main/map-main.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {RouterModule} from '@angular/router';
 import { SideNavComponent } from './side-nav/side-nav.component';
 import { AddMaintenanceComponent } from './Maintenance/add-maintenance/add-maintenance.component';
-import {MatAutocompleteModule, MatInputModule, MatSelectModule} from '@angular/material';
+import {MatAutocompleteModule, MatInputModule, MatSelectModule, MatTableModule, MatPaginatorModule} from '@angular/material';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
@@ -15,7 +15,12 @@ import { SpecComponent } from './Specification/spec/spec.component';
 import { SpecLogTableComponent } from './Specification/spec-log-table/spec-log-table.component';
 import { SpecViewComComponent } from './Specification/spec-view-com/spec-view-com.component';
 import { ViewMaintenanceComponent } from './Maintenance/view-maintenance/view-maintenance.component';
-
+import {MapComponent} from './SiteMap/map/map.component';
+import {RemarksComponent} from './SiteMap/remarks/remarks.component';
+import {SearchMapComponent} from './SiteMap/search-map/search-map.component';
+import {SiteStatusComponent} from './SiteMap/site-status/site-status.component';
+import {StatusReportComponent} from './SiteMap/status-report/status-report.component';
+import { AgmCoreModule } from '@agm/core';
 @NgModule({
   declarations: [
     AppComponent,
@@ -25,7 +30,13 @@ import { ViewMaintenanceComponent } from './Maintenance/view-maintenance/view-ma
     SpecComponent,
     SpecLogTableComponent,
     SpecViewComComponent,
-    ViewMaintenanceComponent
+    ViewMaintenanceComponent,
+    MapMainComponent,
+    MapComponent,
+    RemarksComponent,
+    SearchMapComponent,
+    SiteStatusComponent,
+    StatusReportComponent,
   ],
   imports: [
     BrowserModule,
@@ -38,6 +49,9 @@ import { ViewMaintenanceComponent } from './Maintenance/view-maintenance/view-ma
     MatAutocompleteModule,
     MatInputModule,
     BrowserAnimationsModule,
+    MatTableModule,
+    MatPaginatorModule,
+    AgmCoreModule.forRoot({apiKey: 'AIzaSyAQicz7zm0Cri2pfDdgkh801ae)puYzmuE'}),
   ],
   providers: [],
   bootstrap: [AppComponent]
